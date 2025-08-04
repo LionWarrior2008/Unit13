cc1b0287fc2fe6ac99b74b3cfa61041e8999bd3e
LionWarrior2008 <nrajeev@student.cscc.edu>
2025-08-03 21:34:37 -0400

    hud

--- Hud.py
+++ Hud.py
@@ -19,8 +19,26 @@ class Hud:
         self._update_high_score()
         self._update_max_score()
     def _update_score(self):
-        score_str=f'Score
-
+        score_str=f'Score:{self.game_stats.score: ,.0f}'
+        self.score_image=self.font.render(score_str,True,self.settings.text_color,None)
+        self.score_rect=self.score_image.get_rect()
+        self.score_rect.right=self.bounderes-self.padding
+        self.score_rect.top=self.max_score_rect.bottom+self.padding
+    def _update_max_score(self):
+        max_score_str=f'Max-Score:{self.game_stats.max_score: ,.0f}'
+        self.max_score_image=self.font.render(max_score_str,True,self.settings.text_color,None)
+        self.max_score_rect=self.max_score_image.get_rect()
+        self.max_score_rect.right=self.bounderes-self.padding
+        self.max_score_rect.top=self.padding
+    def _update_max_score(self):
+        High_score_str=f'High-Score:{self.game_stats.high_score: ,.0f}'
+        self.High_score_image=self.font.render(High_score_str,True,self.settings.text_color,None)
+        self.High_score_rect=self.High_score_image.get_rect()
+        self.High_score_rect.midtop=(self.bounderes.centerx,self.padding)
+    def draw(self):
+        self.screen.blit(self.High_score_image,self.High_score_rect)
+        self.screen.blit(self.max_score_image,self.max_score_rect)
+        self.screen.blit(self.score_image,self.score_rect)
 
 
 
